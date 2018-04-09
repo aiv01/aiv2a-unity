8624ada6b565bcb226673db05c475900ded38c99
Roberto De Ioris <roberto@20tab.com>
2018-04-09 18:59:27 +0200

    added sceneloader

---
+++ Assets/AivSceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AivSceneLoader : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        SceneManager.LoadScene("Foo", LoadSceneMode.Additive);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
